9a633c6eec2841f3d9db70dcc0ad066d0abcb337
LuissClass <luisesetbanv71@gmail.com>
2025-10-07 09:20:29 +0200

    Add files

---
+++ PuntoA5.java
@@ -0,0 +1,58 @@
+public class PuntoA5 {
+    private double x;
+    private double y;
+    private final static PuntoA5 ptOrigen = new PuntoA5(0,0);
+
+    PuntoA5 (double x, double y) {
+        this.x=x;
+        this.y=y;
+    }
+
+    public void moverX (double incremento) {
+        x+=incremento;
+    }
+
+    public void moverY (double incremento) {
+        y+=incremento;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public static double discantiaEntrePuntos(PuntoA5 pt1, PuntoA5 pt2) {
+        // Pitagoras: hipotenua = raiz de suma de catetos al cuadrado
+        double cateto1 = pt2.x - pt1.x;
+        double cateto2 = pt2.y - pt1.y;
+        return Math.sqrt((cateto1*cateto1)+(cateto2*cateto2));
+    }
+
+    public static PuntoA5 puntoCercanoAlOrigen(PuntoA5 pt1, PuntoA5 pt2) {
+        if (discantiaEntrePuntos(pt1, ptOrigen) > discantiaEntrePuntos(pt2, ptOrigen)) {
+            return pt2;
+        }
+        return pt1;
+    }
+
+    public void mostrarPunto() {
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+    }
+
+
+    public static void main(String[] args) {
+        PuntoA5 B = new PuntoA5(5, 5);
+        PuntoA5 A = new PuntoA5(0,1);
+        System.out.println("Dstancia");
+        System.out.println(discantiaEntrePuntos(A,B));
+        System.out.println("Punto A: ");
+        A.mostrarPunto();
+        System.out.println("Punto B: ");
+        B.mostrarPunto();
+        System.out.println("Ultima func");
+        puntoCercanoAlOrigen(A,B).mostrarPunto();
+    }
+}
---
+++ TrianguloA6.java
@@ -0,0 +1,96 @@
+public class TrianguloA6 {
+    private PuntoA5 vert1;
+    private PuntoA5 vert2;
+    private PuntoA5 vert3 = new PuntoA5(0,0);
+    double lado1;
+    double lado2;
+    double lado3;
+    boolean hayPerimetro = false;
+
+    TrianguloA6 (PuntoA5 vert1, PuntoA5 vert2) {
+        this.vert1 = vert1;
+        this.vert2 = vert2;
+    }
+
+    TrianguloA6 (PuntoA5 vert1, PuntoA5 vert2, PuntoA5 vert3) {
+        this.vert1 = vert1;
+        this.vert2 = vert2;
+        this.vert3 = vert3;
+    }
+
+    public double calcularPerimetro() {
+        lado1 = PuntoA5.discantiaEntrePuntos(vert1,vert2);
+        lado2 = PuntoA5.discantiaEntrePuntos(vert2,vert3);
+        lado3 = PuntoA5.discantiaEntrePuntos(vert3,vert1);
+
+        hayPerimetro = true;
+
+        return lado1+lado2+lado3;
+    }
+
+    public void moveXs(double incremento){
+        vert1.moverX(incremento);
+        vert2.moverX(incremento);
+        vert3.moverX(incremento);
+    }
+
+    public boolean esEquilatero() {
+        boolean respuesta = false;
+        if (hayPerimetro) {
+            respuesta = lado1 == lado2 || lado2 == lado3 || lado3 == lado1? true: false;
+        } else {
+            System.out.println("No se puede saber si el triangulo es equilatero si no se ha calulado su perimetro");
+        }
+        return respuesta;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Vert1 (" + vert1.getX() + ", " + vert1.getY() + ")");
+        System.out.println("Vert2 (" + vert2.getX() + ", " + vert2.getY() + ")");
+        System.out.println("Vert3 (" + vert3.getX() + ", " + vert3.getY() + ")");
+    }
+
+
+    public static void main(String[] args) {
+        int incremento = 10;
+
+        System.out.println("\n[Creando triangulo 1]");
+        PuntoA5 A = new PuntoA5(1,1);
+        PuntoA5 B = new PuntoA5(0,1);
+        PuntoA5 C = new PuntoA5(1,0);
+
+        TrianguloA6 tri1 = new TrianguloA6(A,B,C);
+        tri1.mostrarDatos();
+
+        System.out.println("\n[Creando triangulo 2: ]");
+        PuntoA5 D = new PuntoA5(2,0);
+        PuntoA5 E = new PuntoA5(0,69);
+        PuntoA5 F = new PuntoA5(7,7);
+
+        TrianguloA6 tri2 = new TrianguloA6(D,E,F);
+        tri2.mostrarDatos();
+
+        System.out.println();
+        System.out.println("[Moviendo " + incremento +  " unidades a la derecha todos los vertices del triangulo 1]");
+        tri1.moveXs(incremento);
+        tri1.mostrarDatos();
+
+        System.out.println();
+        System.out.println("[Calculando perimetro de triangulo 1]");
+        System.out.println("Perimetro:");
+        System.out.println(tri1.calcularPerimetro());
+
+        System.out.println();
+        System.out.println("[Calculando perimetro de triangulo 2]");
+        System.out.println("Perimetro:");
+        System.out.println(tri2.calcularPerimetro());
+
+        System.out.println();
+        System.out.println("Triangulo 1 es equilatero: " + tri1.esEquilatero());
+        System.out.println();
+        System.out.println("Triangulo 2 es equilatero: " + tri2.esEquilatero());
+
+    }
+}
+
+// Para verificar: https://es.planetcalc.com/9423/
