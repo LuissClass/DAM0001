6c1ded8bc573d39eceb6423a0d184a65573213a6
LuissClass <luisesetbanv71@gmail.com>
2025-10-21 14:09:57 +0200

    Update FechaB2.java
    Add input
    +Optimizado

--- DAM1/FechaB2.java
+++ DAM1/FechaB2.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
+
 public class FechaB2 {
     private int day;
     private int month;
     private int year;
+    private Teclado t = new Teclado();
+    boolean isFormatoA = true;
 
     FechaB2(int day, int month, int year) {
         this.day = day;
@@ -9,8 +13,26 @@ public class FechaB2 {
         this.year = year;
     }
 
+    boolean elegirFormato() throws IOException {
+        System.out.println("Que formato prefieres (A/B)?\n\tA) dd-mm-aaaa\n\tB) dd/mm/aaaa");
+        String res = t.leerString();
+
+        if (res.toUpperCase().equals("A")) {
+            return true;
+        } else if (res.toUpperCase().equals("B")) {
+            return false;
+        } else {
+            System.out.println("Respuesta invalida! \tSe usara el formato A");
+        }
+        return false;
+    }
+
     void mostrarConFormato() {
-        System.out.println(day + "-" + month + "-" + year);
+        if (isFormatoA) {
+            System.out.println("\t" + day + "-" + month + "-" + year);
+        } else {
+            System.out.println("\t" + day + "/" + month + "/" + year);
+        }
     }
 
     boolean esBisiesto() {
@@ -22,11 +44,6 @@ public class FechaB2 {
 
     void pasarDia() {
         switch (month) {
-            case 12:
-                year = day == 31 ? year + 1: year;
-                month = day == 31 ? 1 : month;
-                day = day == 31 ? 0 : day;
-                break;
             case 4, 6, 9, 11:
                 month = day == 30 ? month + 1 : month;
                 day = day == 30 ? 0 : day;
@@ -41,6 +58,8 @@ public class FechaB2 {
                 }
                 break;
             default:
+                year = month == 12 && day == 31? year + 1 : year;
+                month = month == 12 && day == 31 ? 0 : month;
                 month = day == 31 ? month + 1 : month;
                 day = day == 31 ? 0 : day;
                 break;
@@ -48,8 +67,9 @@ public class FechaB2 {
         day += 1;
     }
 
-    public static void main(String[] args) {
-        FechaB2 fecha1 = new FechaB2(29, 12, 2024);
+    public static void main(String[] args) throws IOException {
+        FechaB2 fecha1 = new FechaB2(26, 12, 2024);
+        fecha1.elegirFormato();
         fecha1.mostrarConFormato();
         fecha1.pasarDia();
         fecha1.mostrarConFormato();
@@ -65,4 +85,3 @@ public class FechaB2 {
         fecha1.mostrarConFormato();
     }
 }
-
