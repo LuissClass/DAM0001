660acf14c12c1abfe2cd0a50ab5bc30b2f230cb0
LuissClass <luisesetbanv71@gmail.com>
2025-11-20 13:09:04 +0100

    Add files via upload

--- DAM1/EjerArrD/contarNumsDe1DigitoD5.java
+++ DAM1/EjerArrD/contarNumsDe1DigitoD5.java
@@ -18,21 +18,54 @@ public class contarNumsDe1DigitoD5 {
             System.out.println("\n\tTe quedan infinitos numeros para introducir.\n\tIntroduce num<0 para terminar.");
             res = input.nextInt();
             if (res > 0) {
-                addNum(res);
+                if (res < 10) {
+                    addNum(res);
+                } else {
+                    System.out.println("EL NUMERO " + res + " NO SE HA PROCESADO.");
+                }
             }
-        } while (res > 0 && pos < arr.length + 1);
+
+        } while (res > 0 && pos <= arr.length);
         imprimirNumeros();
     }
 
 
     void imprimirNumeros() {
-        for (int i = 0; i < pos; i++) {
-            System.out.println("El " + arr[i] + " se ha introducido " + contarNum(arr[i]) + " veces.");
+        int[] numerosAparecidos = new int[arr.length];
+        int num;
+        int posNumApr = 0;
+        boolean haAparecido;
+        for (int i = 0; i < arr.length; i++) {
+            num = arr[i];
+            haAparecido = false;
+
+            for (int j = 0; j < numerosAparecidos.length; j++) {
+                if (num == numerosAparecidos[j]) {
+                    haAparecido = true;
+                }
+            }
+
+            if (!haAparecido) {
+                numerosAparecidos[posNumApr] = num;
+                posNumApr++;
+            }
+        }
+
+        for (int i = 0; i < posNumApr; i++) {
+            System.out.println("El " + numerosAparecidos[i] + " se ha introducido " + contarNum(numerosAparecidos[i]) + " veces.");
         }
     }
 
     void crecerArray() {
-        arr = new int[arr.length + 1];
+        if (arr != null) {
+            int[] arrAux = arr;
+            arr = new int[arr.length + 1];
+            for (int i = 0; i < pos; i++) {
+                arr[i] = arrAux[i];
+            }
+        } else {
+            int[] arr = new int[1];
+        }
     }
 
     void addNum(int num) {
